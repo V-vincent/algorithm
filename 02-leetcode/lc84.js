b77f5e7dd9082a93c148a63c4b0b7ebd4d33def7
vincent <1131182508@qq.com>
2021-03-10 18:28:50 +0800

    LeetCode84. 柱状图中最大的矩形-解决

--- 02-leetcode/lc84.js
+++ 02-leetcode/lc84.js
@@ -11,6 +11,8 @@
 // 输入: [2,1,5,6,2,3]
 // 输出: 10
 
+// 思路：
+// 从某一点开始，往左右两边找，比当前元素小就终止
 let heights = [2, 1, 5, 6, 2, 3];
 let h1 = [1];
 let h2 = [1, 1, 1];
@@ -32,8 +34,35 @@ var largestRectangleArea = function (heights) {
   }
   return max;
 };
-largestRectangleArea(heights);
-largestRectangleArea(h1);
-largestRectangleArea(h2);
+// largestRectangleArea(heights);
+// largestRectangleArea(h1);
+// largestRectangleArea(h2);
 // 。。。超出时间限制了
 
+// 单调栈：栈中元素按递增顺序或者递减顺序排列；最大好处就是时间复杂度是线性的；每个元素遍历一次!
+// 木桶原理，找到最短的那块木板
+let heights1 = [2, 1, 5, 6, 2, 3];
+var largestRectangleArea = function (heights) {
+  if (heights.length <= 0) return 0;
+  let max = 0;
+  // 加这一行：考虑到只有一个元素([1])，或者所有元素都相同时([1, 1, 1])会出错
+  heights = [0, ...heights, 0];
+  // 用栈来记录数组元素的下标
+  let stack = [];
+  for (let i = 0; i < heights.length; i++) {
+    // 当前元素和栈顶元素比较，如果当前元素小于栈顶元素，则栈顶元素出栈
+    // 并求以出栈元素为中心时，它的最大面积
+    while (heights[i] < heights[stack[stack.length - 1]]) {
+      let index = stack.pop(); // 取出栈顶元素
+      // 以出栈元素为中心求面积：当前元素（i）与栈顶元素（stack.length - 1）之间都是大于等于栈顶元素的值
+      let tempArea = heights[index] * (i - stack[stack.length - 1] - 1);
+      // console.log(`当前元素：${i}。出栈元素：下标：${index}，值：${heights[index]}，面积：${tempArea}`);
+      max = Math.max(max, tempArea);
+    }
+    // 当前元素大于栈顶元素或栈没有元素时，入栈
+    stack.push(i);
+    // console.log('下标栈stack：', stack);
+  }
+  return max;
+};
+largestRectangleArea(heights);
