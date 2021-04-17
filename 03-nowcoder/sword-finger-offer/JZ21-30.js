f09baf578f2c0c044cbc8769037c2d73d869d5fb
vincent <32922167+V-vincent@users.noreply.github.com>
2021-04-17 23:56:46 +0800

    add jz25

--- 03-nowcoder/sword-finger-offer/JZ21-30.js
+++ 03-nowcoder/sword-finger-offer/JZ21-30.js
@@ -123,3 +123,33 @@ function FindPath(root, expectNumber) {
   dfs(root, [])
   return res;
 }
+
+// JZ25	复杂链表的复制	链表	较难
+// 题目描述
+// 输入一个复杂链表（每个节点中有节点值，以及两个指针，一个指向下一个节点，另一个特殊指针random指向一个随机节点），
+// 请对此链表进行深拷贝，并返回拷贝后的头结点。
+// （注意，输出结果中请不要返回参数中的节点引用，否则判题程序会直接返回空）
+function RandomListNode(x) {
+  this.label = x;
+  this.next = null;
+  this.random = null;
+}
+function Clone(pHead) {
+  if (!pHead || !pHead.next) return pHead;
+  let cur = pHead;
+  let map = new Map();
+  // 复制每一个节点并存储
+  while (cur) {
+    let node = new RandomListNode(cur.label);
+    map.set(cur, node);
+    cur = cur.next;
+  }
+  cur = pHead;
+  // 把节点连起来
+  while (cur) {
+    map.get(cur).next = map.get(cur.next);
+    map.get(cur).random = map.get(cur.random);
+    cur = cur.next
+  }
+  return map.get(pHead);
+}
