350fb87e6c4cca73ad56827a47446adf6d4bac42
vincent <32922167+V-vincent@users.noreply.github.com>
2021-04-17 17:34:40 +0800

    冒泡排序优化

--- 04-other/01-arr.js
+++ 04-other/01-arr.js
@@ -18,10 +18,10 @@ function concatArr(arr1, arr2) {
   }
   return res;
 }
-console.log(concatArr(arr1, arr2));
+// console.log(concatArr(arr1, arr2));
 
 // 使用迭代的方式实现 flatten 函数
-let arr = [1, 2, 3, [4, 5], [6, [7, [8]]]];
+let flattenArr = [1, 2, 3, [4, 5], [6, [7, [8]]]];
 function flatten(arr) {
   while (arr.some(item => Array.isArray(item))) {
     arr = [].concat(...arr)
@@ -40,4 +40,44 @@ function flatten1(arr) {
   })
   return arrs
 }
-console.log(flatten1(arr));
+// console.log(flatten1(flattenArr));
+
+// 冒泡排序如何实现，时间复杂度是多少， 还可以如何改进？
+// 时间复杂度：O(n^2)
+let buddleArr = [5, 2, 7, 8, 1, 3, 6];
+// 实现1：
+function buddleSort(arr) {
+  let len = arr.length;
+  while (len > 1) {
+    for (let i = 0; i < len - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+      }
+    }
+    len--;
+  }
+  return arr;
+}
+// 优化：
+function buddleSort1(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    // 从左到右排
+    for (let i = left; i < right; i++) {
+      if (arr[i] > arr[i + 1]) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+      }
+    }
+    right--;
+    // 从右往左排
+    for (let i = right; i > left; --i) {
+      if (arr[i] < arr[i - 1]) {
+        [arr[i], arr[i - 1]] = [arr[i - 1], arr[i]];
+      }
+    }
+    left++;
+  }
+  return arr;
+}
+console.log(buddleSort1(buddleArr))
