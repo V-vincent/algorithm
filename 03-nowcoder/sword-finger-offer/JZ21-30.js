31916bf114db3c9433c654f50c849543150414ba
vincent <32922167+V-vincent@users.noreply.github.com>
2021-04-18 16:53:06 +0800

    JZ27

--- 03-nowcoder/sword-finger-offer/JZ21-30.js
+++ 03-nowcoder/sword-finger-offer/JZ21-30.js
@@ -153,3 +153,28 @@ function Clone(pHead) {
   }
   return map.get(pHead);
 }
+
+// JZ27	字符串的排列	字符串动态规划	较难
+// 题目描述
+// 输入一个字符串,按字典序打印出该字符串中字符的所有排列。例如输入字符串abc,则按字典序打印出由字符a,b,c所能排列出来的所有字符串abc,acb,bac,bca,cab和cba。
+// 输入描述:
+// 输入一个字符串,长度不超过9(可能有字符重复),字符只包括大小写字母。
+// 示例1
+// 输入
+// "ab"
+// 返回值
+// ["ab","ba"]
+// 递归实现
+function Permutation(str) {
+  let set = new Set();
+  let dfs = (pre, str) => {
+    if (str.length === 0) {
+      return set.add(pre + str);
+    }
+    for (let i = 0; i < str.length; i++) {
+      dfs(pre + str[i], str.slice(0, i) + str.slice(i + 1))
+    }
+  }
+  dfs('', str);
+  return Array.from(set);
+}
