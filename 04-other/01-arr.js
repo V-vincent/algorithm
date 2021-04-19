d764f8a408ee9ecbf9177160db47184fa8ef40e4
vincent <1131182508@qq.com>
2021-04-19 11:41:59 +0800

    给定两个数组，写一个方法来计算它们的交集

--- 04-other/01-arr.js
+++ 04-other/01-arr.js
@@ -22,3 +22,51 @@ function concatArr(arr1, arr2) {
 
 // 给定两个数组，写一个方法来计算它们的交集
 // 例如：给定 nums1 = [1, 2, 2, 1]，nums2 = [2, 2]，返回 [2, 2]。
+// 疑问？求数组的交集还是元素的交集？
+let nums1 = [1, 2, 2, 1];
+let nums2 = [2, 2];
+// 数组的交集
+// function getArrIntersect(nums1, nums2) {
+//   let res = [];
+//   if (!nums1.length || !nums2.length) return res;
+//   for (let i = 0; i < nums1.length; i++) {
+//     for (let j = 0; j < nums2.length; j++) {
+//       if (nums1[i] == nums2[j]) {
+//         return findSameVal(i, j, nums1, nums2)
+//       }
+//     }
+//   }
+//   return res;
+// }
+// // 按顺序对比，相等加入，不相等退出循环
+// function findSameVal(cur1, cur2, nums1, nums2) {
+//   let temp = [];
+//   while (cur1 < nums1.length && cur2 < nums2.length && nums1[cur1] === nums2[cur2]) {
+//     temp.push(nums1[cur1])
+//     cur1++;
+//     cur2++;
+//   }
+//   return temp;
+// }
+
+// 元素的交集
+function getArrIntersect(nums1, nums2) {
+  if (!nums1.length || !nums2.length) return [];
+  let res = [];
+  let map = {};
+  for (let i = 0; i < nums1.length; i++) {
+    if (map[nums1[i]]) {
+      map[nums1[i]]++;
+    } else {
+      map[nums1[i]] = 1;
+    }
+  }
+  for (let i = 0; i < nums2.length; i++) {
+    if (map[nums2[i]] > 0) {
+      res.push(nums2[i]);
+      map[nums2[i]]--;
+    }
+  }
+  return res;
+}
+console.log(getArrIntersect(nums1, nums2))
