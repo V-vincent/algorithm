ef716c68b3a0bf7d3662387da2aa13b510a4b625
vincent <1131182508@qq.com>
2021-04-19 15:57:26 +0800

    JZ30	连续子数组的最大和

--- 03-nowcoder/sword-finger-offer/JZ21-30.js
+++ 03-nowcoder/sword-finger-offer/JZ21-30.js
@@ -216,3 +216,23 @@ function GetLeastNumbers_Solution(input, k) {
   let res = input.sort((a, b) => a - b).splice(0, k)
   return res;
 }
+
+// JZ30	连续子数组的最大和	动态规划分治	简单
+// 题目描述
+// 输入一个整型数组，数组里有正数也有负数。数组中的一个或连续多个整数组成一个子数组。求所有子数组的和的最大值。要求时间复杂度为 O(n).
+// 示例1
+// 输入
+// [1,-2,3,10,-4,7,2,-5]
+// 返回值
+// 18
+// 说明
+// 输入的数组为{1,-2,3,10,—4,7,2,一5}，和最大的子数组为{3,10,一4,7,2}，因此输出为该子数组的和 18。 
+function FindGreatestSumOfSubArray(array) {
+  let max = array[0];
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum = (sum + array[i]) > array[i] ? (sum + array[i]) : array[i];
+    max = sum > max ? sum : max;
+  }
+  return max;
+}
