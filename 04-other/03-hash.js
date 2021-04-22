88b3583543b5b139d6a28f1f6f5a57efca9cf58f
vincent <1131182508@qq.com>
2021-04-22 10:36:56 +0800

    字符串匹配

--- 04-other/03-hash.js
+++ 04-other/03-hash.js
@@ -19,3 +19,16 @@ function wordNegation1(word) {
   }
   return res;
 }
+
+// 实现一个字符串匹配算法，从长度为 n 的字符串 S 中，查找是否存在字符串 T，T 的长度是 m，若存在返回所在位置。
+function find(S, T) {
+  let n = S.length;
+  let m = T.length;
+  if (n < m) return -1;
+  for (let i = 0; i < n - m; i++) {
+    if (S.slice(i, i + m) === T) return i;
+  }
+  return -1;
+}
+// search() 方法用于检索字符串中指定的子字符串，或检索与正则表达式相匹配的子字符串
+let find1 = (S, T) => S.search(T);
