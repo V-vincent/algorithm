9cd895374ce530541221d4e0f101ed00a906f2af
vincent <1131182508@qq.com>
2021-04-22 16:26:16 +0800

    旋转数组

--- 04-other/01-arr.js
+++ 04-other/01-arr.js
@@ -91,8 +91,30 @@ function randomNewArr() {
   }
   // 输出数组
   let res = [];
-  for(let key in obj) {
+  for (let key in obj) {
     res.push(obj[key])
   }
   return res;
+}
+
+// 算法题「旋转数组」
+// 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数
+// 示例 1：
+// 输入: [1, 2, 3, 4, 5, 6, 7] 和 k = 3
+// 输出: [5, 6, 7, 1, 2, 3, 4]
+// 解释:
+// 向右旋转 1 步: [7, 1, 2, 3, 4, 5, 6]
+// 向右旋转 2 步: [6, 7, 1, 2, 3, 4, 5]
+// 向右旋转 3 步: [5, 6, 7, 1, 2, 3, 4]
+
+// 示例 2：
+// 输入: [-1, -100, 3, 99] 和 k = 2
+// 输出: [3, 99, -1, -100]
+// 解释: 
+// 向右旋转 1 步: [99, -1, -100, 3]
+// 向右旋转 2 步: [3, 99, -1, -100]
+function rotateArr(arr, k) {
+  let len = arr.length;
+  k = k % len;
+  return arr.slice(len - k).concat(arr.slice(0, len - k));
 }
