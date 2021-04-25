66fd1ca46861cbefbaff41bf7cc748f43d2625c2
vincent <1131182508@qq.com>
2021-04-25 15:41:45 +0800

    JZ38  二叉树的深度

--- 03-nowcoder/sword-finger-offer/JZ31-40.js
+++ 03-nowcoder/sword-finger-offer/JZ31-40.js
@@ -151,3 +151,18 @@ function GetNumberOfK(data, k) {
   }
   return leftB - leftS;
 }
+
+// JZ38	二叉树的深度	树	简单
+// 题目描述
+// 输入一棵二叉树，求该树的深度。从根结点到叶结点依次经过的结点（含根、叶结点）形成树的一条路径，最长路径的长度为树的深度。
+// 示例1
+// 输入
+// {1,2,3,4,5,#,6,#,#,7}
+// 返回值
+// 4
+function TreeDepth(pRoot) {
+  if (!pRoot) return 0;
+  let left = TreeDepth(pRoot.left);
+  let right = TreeDepth(pRoot.right);
+  return Math.max(left, right) + 1;
+}
