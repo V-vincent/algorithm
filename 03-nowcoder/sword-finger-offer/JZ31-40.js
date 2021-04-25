4eb7220b8697a5bf795ef851273ab61479ebbcbf
vincent <1131182508@qq.com>
2021-04-25 14:57:20 +0800

    JZ36 两个链表的第一个公共结点

--- 03-nowcoder/sword-finger-offer/JZ31-40.js
+++ 03-nowcoder/sword-finger-offer/JZ31-40.js
@@ -98,3 +98,22 @@ function FirstNotRepeatingChar(str) {
   }
   return -1;
 }
+
+// JZ36	两个链表的第一个公共结点	链表	中等
+// 题目描述
+// 输入两个链表，找出它们的第一个公共结点。
+// （注意因为传入数据是链表，所以错误测试数据的提示是用其他方式显示的，保证传入数据是正确的）
+function FindFirstCommonNode(pHead1, pHead2) {
+  if (!pHead1 || !pHead2) return null;
+  let p1 = pHead1, p2 = pHead2;
+  // 双指针法
+  // 初始化：指针p1指向链表pHead1头结点，指针p2指向链表pHead2头结点
+  // 如果p1 == p2， 说明找到了第一个公共的头结点，直接返回即可。
+  // 否则，p1 != p2，则++p1，++p2
+  while (p1 !== p2) {
+    // 如果链表长度不相同
+    p1 = p1 ? p1.next : pHead2; // p1 + p2作为链表pHead1的新长度
+    p2 = p2 ? p2.next : pHead1; // p2 + p1作为链表pHead2的新长度
+  }
+  return p1;
+}
