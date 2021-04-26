455b8c21ce2c5bd7965f30af944b39483cb83f65
vincent <1131182508@qq.com>
2021-04-26 15:17:38 +0800

    JZ39	平衡二叉树

--- 03-nowcoder/sword-finger-offer/JZ31-40.js
+++ 03-nowcoder/sword-finger-offer/JZ31-40.js
@@ -166,3 +166,20 @@ function TreeDepth(pRoot) {
   let right = TreeDepth(pRoot.right);
   return Math.max(left, right) + 1;
 }
+
+// JZ39	平衡二叉树	树dfs	简单
+// 题目描述
+// 输入一棵二叉树，判断该二叉树是否是平衡二叉树。
+// 在这里，我们只需要考虑其平衡性，不需要考虑其是不是排序二叉树
+// 平衡二叉树（Balanced Binary Tree），具有以下性质：它是一棵空树或它的左右两个子树的高度差的绝对值不超过1，并且左右两个子树都是一棵平衡二叉树。
+// 示例1
+// 输入
+// {1,2,3,4,5,6,7}
+// 返回值
+// true
+function IsBalanced_Solution(pRoot) {
+  if (!pRoot) return true;
+  let left = TreeDepth(pRoot.left);
+  let right = TreeDepth(pRoot.right);
+  return Math.abs(left - right) <= 1;
+}
