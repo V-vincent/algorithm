e549f05309264204762eb1cbceea9ab7bce9ab92
vincent <1131182508@qq.com>
2021-04-27 18:21:46 +0800

    JZ42	和为S的两个数字

--- 03-nowcoder/sword-finger-offer/JZ41-50.js
+++ 03-nowcoder/sword-finger-offer/JZ41-50.js
@@ -34,3 +34,33 @@ function FindContinuousSequence(sum) {
   }
   return res;
 }
+
+// JZ42	和为S的两个数字	双指针数组	中等
+// 题目描述
+// 输入一个递增排序的数组和一个数字S，在数组中查找两个数，使得他们的和正好是S，如果有多对数字的和等于S，输出两个数的乘积最小的。
+// 返回值描述:
+// 对应每个测试案例，输出两个数，小的先输出。
+// 示例1
+// 输入
+// [1,2,4,7,11,15],15
+// 返回值
+// [4,11]
+function FindNumbersWithSum(array, sum) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] >= sum) return [];
+    let temp = sum - array[i];
+    if (array.indexOf(temp) > -1) {
+      return [array[i], temp]
+    }
+  }
+  return [];
+}
+function FindNumbersWithSum2(array, sum) {
+  let left = 0, right = array.length - 1;
+  while (left < right) {
+    if (array[left] + array[right] > sum) right--;
+    else if (array[left] + array[right] < sum) left++;
+    else return [array[left], array[right]]
+  }
+  return [];
+}
