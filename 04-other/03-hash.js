fe2955a760d63ebf093c5e5bb0efa47592787dc3
vincent <1131182508@qq.com>
2021-04-30 16:53:23 +0800

    整数逆序

--- 04-other/03-hash.js
+++ 04-other/03-hash.js
@@ -39,3 +39,17 @@ function getSymmetryNum() {
     return item > 10 && item === Number(item.toString().split('').reverse().join(''));
   })
 }
+
+
+// 用 JavaScript 写一个函数，输入 int 型，返回整数逆序后的字符串。如：输入整型 1234，返回字符串“4321”。
+// 要求必须使用递归函数调用，不能用全局变量，输入函数必须只有一个参数传入，必须返回字符串。
+function dfsReserve(num) {
+  let num1 = num / 10;
+  let num2 = num % 10;
+  if (num1 < 1) {
+    return num2;
+  } else {
+    let temp = Math.floor(num1);
+    return `${num2}${dfsReserve(temp)}`
+  }
+}
