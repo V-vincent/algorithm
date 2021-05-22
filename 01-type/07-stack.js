51c853ee7208fb5317441a43b7ae216e3aa5d690
vincent <32922167+V-vincent@users.noreply.github.com>
2021-05-22 15:20:06 +0800

    栈的实现

---
+++ 01-type/07-stack.js
@@ -0,0 +1,43 @@
+class Stack {
+  constructor() {
+    this.stack = []
+  }
+  push(item) {
+    this.stack.push(item)
+  }
+  pop() {
+    this.stack.pop()
+  }
+  peek() {
+    return this.stack[this.getCount() - 1]
+  }
+  getCount() {
+    return this.stack.length
+  }
+  isEmpty() {
+    return this.getCount() === 0
+  }
+}
+
+// 匹配括号，可以通过栈的特性
+var isValid = function (s) {
+  let map = {
+    '(': -1,
+    ')': 1,
+    '[': -2,
+    ']': 2,
+    '{': -3,
+    '}': 3
+  }
+  let stack = []
+  for (let i = 0; i < s.length; i++) {
+    if (map[s[i]] < 0) {
+      stack.push(s[i])
+    } else {
+      let last = stack.pop()
+      if (map[last] + map[s[i]] != 0) return false
+    }
+  }
+  if (stack.length > 0) return false
+  return true
+};
