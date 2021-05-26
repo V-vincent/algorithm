75b4c44b9e3b8d4e2d91b6f42cfd984ce6e14e99
vincent <1131182508@qq.com>
2021-05-26 16:50:15 +0800

    43左旋转字符串、44翻转单词序列

--- 03-nowcoder/sword-finger-offer/JZ41-50.js
+++ 03-nowcoder/sword-finger-offer/JZ41-50.js
@@ -64,3 +64,29 @@ function FindNumbersWithSum2(array, sum) {
   }
   return [];
 }
+
+// JZ43  左旋转字符串   字符串   中等
+// 描述
+// 汇编语言中有一种移位指令叫做循环左移（ROL），现在有个简单的任务，就是用字符串模拟这个指令的运算结果。对于一个给定的字符序列S，请你把其循环左移K位后的序列输出。例如，字符序列S=”abcXYZdef”,要求输出循环左移3位后的结果，即“XYZdefabc”。是不是很简单？OK，搞定它！
+// 示例1
+// 输入：
+// "abcXYZdef",3
+// 返回值：
+// "XYZdefabc"
+function LeftRotateString(str, n) {
+  if (!str) return "";
+  let num = n % str.length;
+  return str.slice(num) + str.slice(0, num);
+}
+
+// JZ44  翻转单词序列 字符串 较难
+// 描述
+// 牛客最近来了一个新员工Fish，每天早晨总是会拿着一本英文杂志，写些句子在本子上。同事Cat对Fish写的内容颇感兴趣，有一天他向Fish借来翻看，但却读不懂它的意思。例如，“nowcoder. a am I”。后来才意识到，这家伙原来把句子单词的顺序翻转了，正确的句子应该是“I am a nowcoder.”。Cat对一一的翻转这些单词顺序可不在行，你能帮助他么？
+// 示例1
+// 输入：
+// "nowcoder. a am I"
+// 返回值：
+// "I am a nowcoder."
+function ReverseSentence(str) {
+  return str.split(' ').reverse().join(' ')
+}
