3df795c99b70fbdd2683e29486b8e2dc4aa5dabd
vincent <32922167+V-vincent@users.noreply.github.com>
2021-06-12 20:58:17 +0800

    JZ50 数组中重复的数字

--- 03-nowcoder/sword-finger-offer/JZ41-50.js
+++ 03-nowcoder/sword-finger-offer/JZ41-50.js
@@ -191,3 +191,25 @@ function Sum_Solution(n) {
   if (n == 1) return 1;
   return n + Sum_Solution(n - 1)
 }
+
+// JZ50 数组中重复的数字 简单
+// 描述
+// 在一个长度为n的数组里的所有数字都在0到n-1的范围内。 数组中某些数字是重复的，但不知道有几个数字是重复的。也不知道每个数字重复几次。请找出数组中任一一个重复的数字。 例如，如果输入长度为7的数组[2,3,1,0,2,5,3]，那么对应的输出是2或者3。存在不合法的输入的话输出-1
+// 示例1
+// 输入：
+// [2,3,1,0,2,5,3]
+// 返回值：
+// 2
+// 说明：
+// 2或3都是对的 
+function duplicate(numbers) {
+  let obj = {};
+  for (let i = 0; i < numbers.length; i++) {
+    if (obj[numbers[i]]) {
+      return numbers[i];
+    } else {
+      obj[numbers[i]] = 1;
+    }
+  }
+  return -1;
+}
