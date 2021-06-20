97ae241a6b3855c8601147cf4a7ac2192af2fea2
vincent <32922167+V-vincent@users.noreply.github.com>
2021-06-20 19:38:39 +0800

    4. 寻找两个正序数组的中位数

---
+++ 02-leetcode/1-50/lc4.js
@@ -0,0 +1,62 @@
+// 4. 寻找两个正序数组的中位数
+// 给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的 中位数 。
+
+// 示例 1：
+// 输入：nums1 = [1,3], nums2 = [2]
+// 输出：2.00000
+// 解释：合并数组 = [1,2,3] ，中位数 2
+
+// 示例 2：
+// 输入：nums1 = [1,2], nums2 = [3,4]
+// 输出：2.50000
+// 解释：合并数组 = [1,2,3,4] ，中位数 (2 + 3) / 2 = 2.5
+
+// 示例 3：
+// 输入：nums1 = [0,0], nums2 = [0,0]
+// 输出：0.00000
+
+// 示例 4：
+// 输入：nums1 = [], nums2 = [1]
+// 输出：1.00000
+
+// 示例 5：
+// 输入：nums1 = [2], nums2 = []
+// 输出：2.00000
+
+
+// 提示：
+// nums1.length == m
+// nums2.length == n
+// 0 <= m <= 1000
+// 0 <= n <= 1000
+// 1 <= m + n <= 2000
+// -106 <= nums1[i], nums2[i] <= 106
+
+// 进阶：你能设计一个时间复杂度为 O(log (m+n)) 的算法解决此问题吗？
+var findMedianSortedArrays = function (nums1, nums2) {
+  let res = [...nums1, ...nums2].sort((a, b) => a - b);
+  if (res.length % 2 === 0) {
+    return (res[res.length / 2] + res[res.length / 2 - 1]) / 2;
+  } else {
+    return res[Math.floor(res.length / 2)]
+  }
+};
+
+// 空间复杂度为 O(1)
+// 双指针
+// [1, 2, 3, 5] [2, 3, 4, 6]
+var findMedianSortedArrays = function (nums1, nums2) {
+  let len = nums1.length + nums2.length;
+  let [index1, index2, preVal, curVal] = [0, 0, -1, -1];
+  let mid = Math.floor(len / 2);
+  for (let i = 0; i <= mid; i++) {
+    preVal = curVal;
+    if (index1 >= nums1.length || nums1[index1] > nums2[index2]) {
+      curVal = nums2[index2++];
+    } else {
+      curVal = nums1[index1++];
+    }
+  }
+  return len % 2 === 0 ? (preVal + curVal) / 2 : curVal;
+}
+// console.log(findMedianSortedArrays([1, 2], [3, 4]))
