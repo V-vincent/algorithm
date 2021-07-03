4ae35243db98451eea796d0ec9e967885b3a0f52
vincent <32922167+V-vincent@users.noreply.github.com>
2021-07-04 01:25:39 +0800

    双指针

--- 01-type/double-pointer.js
+++ 01-type/double-pointer.js
@@ -1,9 +1,10 @@
+// 双指针解法
+
 // 11. 盛最多水的容器 难度 中等
 // https://leetcode-cn.com/problems/container-with-most-water/
 // 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0) 。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
 
 // 说明：你不能倾斜容器。
-
 // 示例 1：
 // 输入：[1,8,6,2,5,4,8,3,7]
 // 输出：49 
@@ -25,12 +26,6 @@
 // n = height.length
 // 2 <= n <= 3 * 104
 // 0 <= height[i] <= 3 * 104
-
-// 双指针解法
-/**
- * @param {number[]} height
- * @return {number}
- */
 var maxArea = function (height) {
   if (height.length <= 1) return 0;
   let [left, right] = [0, height.length - 1];
