b0a114c8f58bddc8ac2e7d1f9913b67b348f91f7
V-vincent <1131182508@qq.com>
2022-06-15 19:40:58 +0800

    15-三数之和

--- 02-leetcode/01-array/medium-01.js
+++ 02-leetcode/01-array/medium-01.js
@@ -22,4 +22,33 @@
 // 0 <= nums.length <= 3000
 // -105 <= nums[i] <= 105
 
-
+// 先排序，再左右向中间收拢，注意去重
+var threeSum = function (nums) {
+  let len = nums.length
+  let res = []
+  nums.sort((a, b) => a - b) // 先排序
+  for (let i = 0; i < len - 2; i++) {
+    // 如果当前值大于0则三数之和肯定大于0
+    if (nums[i] > 0) return res
+    // 去重
+    if (i > 0 && nums[i] == nums[i - 1]) continue
+    let left = i + 1
+    let right = len - 1
+    while (left < right) {
+      let sum = nums[i] + nums[left] + nums[right]
+      if (sum == 0) {
+        res.push([nums[i], nums[left], nums[right]])
+        left++
+        right--
+      } else if (sum > 0) {
+        right--
+      } else {
+        left++
+      }
+      // 去重
+      while (left > i + 1 && nums[left] == nums[left - 1]) left++
+      while (right < len - 1 && nums[right] == nums[right + 1]) right--
+    }
+  }
+  return res
+};
