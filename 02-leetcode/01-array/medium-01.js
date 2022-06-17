8f372e082d93bb4d051e38ebfc3f363235d51e0c
V-vincent <1131182508@qq.com>
2022-06-17 20:36:12 +0800

    四数之和

--- 02-leetcode/01-array/medium-01.js
+++ 02-leetcode/01-array/medium-01.js
@@ -98,3 +98,59 @@ const threeSumClosest = function (nums, target) {
   }
   return res
 }
+
+// 18. 四数之和
+// https://leetcode.cn/problems/4sum/
+// 给你一个由 n 个整数组成的数组 nums ，和一个目标值 target 。
+// 请你找出并返回满足下述全部条件且不重复的四元组 [nums[a], nums[b], nums[c], nums[d]] （若两个四元组元素一一对应，则认为两个四元组重复）：
+// 0 <= a, b, c, d < n
+// a、b、c 和 d 互不相同
+// nums[a] + nums[b] + nums[c] + nums[d] == target
+// 你可以按 任意顺序 返回答案 。
+//  
+
+// 示例 1：
+// 输入：nums = [1,0,-1,0,-2,2], target = 0
+// 输出：[[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+
+// 示例 2：
+// 输入：nums = [2,2,2,2,2], target = 8
+// 输出：[[2,2,2,2]]
+
+// 提示：
+// 1 <= nums.length <= 200
+// -109 <= nums[i] <= 109
+// -109 <= target <= 109
+
+// 排序，三数之和外套一层循环
+const fourSum = function (nums, target) {
+  const len = nums.length
+  const res = []
+  nums.sort((a, b) => a - b)
+  for (let i = 0; i < len - 3; i++) {
+    // 去重
+    if (i > 0 && nums[i] === nums[i - 1]) continue
+    for (let j = i + 1; j < len - 2; j++) {
+      // 去重
+      if (j > i + 1 && nums[j] === nums[j - 1]) continue
+      let [left, right] = [j + 1, len - 1]
+      while (left < right) {
+        const sum = nums[i] + nums[j] + nums[left] + nums[right]
+        if (sum > target) {
+          right--
+          continue
+        }
+        if (sum < target) {
+          left++
+          continue
+        }
+        res.push([nums[i], nums[j], nums[left], nums[right]])
+        right--
+        left++
+        while (nums[left] === nums[left - 1]) left++
+        while (nums[right] === nums[right + 1]) right--
+      }
+    }
+  }
+  return res
+};
